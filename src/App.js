dfa3477884986e1d351f61afc0b38bfe5ab717ee
anand-sakre <anand.sakre@vishleshan.net>
2024-02-19 12:52:08 +0530

    new branch

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>demo</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,6 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Hello Everyone. This is me from VSS. Hope youre have a great day!!
+          
         </a>
       </header>
     </div>
